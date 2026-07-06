0e044cb04a1ba69c1fddbdfe53167cf67bbdb695
werapatt-op <27424@bbw.ac.th>
2026-07-06 10:07:07 +0700

    เขียนโปรแกรมสูตรคูณ v.2
    6-07-69

---
+++ เขียนโปรแกรมคำนวณสูตรคูณv.2.py
@@ -0,0 +1,6 @@
+print(">>>โปรแกรมคำนวณสูตรคูณ<<<\n")
+for Multiplication_table in range(2,5):
+    print("\nตารางสูตรคูณของ",Multiplication_table)
+    for i in range(1, 13):
+        print(Multiplication_table, "*", i , "=",Multiplication_table )
+print("วีรภัทร หมื่นแทน ม.4/4 เลขที่28")
