0ab807d31bc03540f3f32db0dfa1a91b294e80d1
werapatt-op <27424@bbw.ac.th>
2026-07-06 09:15:49 +0700

    เขียนโปรแกรมคำนวณสูตรคูณ 6-07-69
    เขียนโปนแกรมคำนวณสูตรคูณ

---
+++ werapat.py
@@ -0,0 +1,5 @@
+print(">>>โปรแกรมคำนวณแม่สูตรคูณ<<<")
+multiplication_table = int(input("ใส่ตัวเลขแม่สูตรคูณ"))
+for i in range(1,13):
+    print(multiplication_table , "*" , i , "=" , multiplication_table*i)
+print("วีรภัทร หมื่นแทน")
